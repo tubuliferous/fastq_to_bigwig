f65aaad66f8604d22012c28e6f1ad7c5eaf055d1
Ben Weaver <tubuliferous@MacBook-Pro-2.attlocal.net>
2023-08-19 21:55:22 -0500

    first commit

---
+++ fastq_to_bigwig.py
@@ -0,0 +1,100 @@
+import argparse
+import subprocess
+import os
+
+def run_command(command):
+    """Run a shell command."""
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode != 0:
+        print(f"Error executing command: {command}")
+        print(stderr.decode())
+        exit(1)
+
+def get_base_name(filename):
+    while '.' in filename:
+        filename = os.path.splitext(filename)[0]
+    return filename
+
+def create_directory(path):
+    """
+    Create a directory specified by the given path.
+    
+    Args:
+    - path (str): The path of the directory to be created.
+    
+    Returns:
+    - None
+    """
+    try:
+        os.mkdir(path)
+        print(f"Directory {path} created successfully!")
+    except FileExistsError:
+        print(f"Directory {path} already exists.")
+    except Exception as e:
+        print(f"Error creating directory {path}. Reason: {e}")
+
+def get_topmost_directory(path):
+    # Split the path into its components
+    parts = os.path.normpath(path).split(os.path.sep)
+    # Return the first directory after the root, if available
+    return parts[1] if len(parts) > 1 else None
+
+def align_fastq(fastq_file, genome_file, aligner_path, output_prefix):
+    """Align FASTQ to produce a SAM file."""
+    sam_file = output_prefix + ".sam"
+    run_command(f"{aligner_path} -x {genome_file} -U {fastq_file} -S {sam_file}")
+    return sam_file
+
+def sam_to_bam(sam_file, output_prefix):
+    """Convert SAM to sorted BAM."""
+    bam_file = output_prefix + ".sorted.bam"
+    run_command(f"samtools view -Sb {sam_file} | samtools sort -o {bam_file}")
+    return bam_file
+
+def generate_bedgraph(bam_file, genome_file, output_prefix):
+    """Generate BedGraph from BAM and sort it."""
+    unsorted_bedgraph_file = output_prefix + ".unsorted.bedgraph"
+    bedgraph_file = output_prefix + ".bedgraph"
+    run_command(f"bedtools genomecov -bg -ibam {bam_file} -g {genome_file} > {unsorted_bedgraph_file}")
+    run_command(f"LC_COLLATE=C sort -k1,1 -k2,2n {unsorted_bedgraph_file} > {bedgraph_file}")
+    return bedgraph_file
+
+def bedgraph_to_bigwig(bedgraph_file, genome_file, output_prefix):
+    """Convert BedGraph to BigWig."""
+    # print("1\n ")
+    chrom_sizes_file = os.path.join(get_topmost_directory(output_prefix), "chrom.sizes")
+    print(chrom_sizes_file)
+    # print("2\n")
+    run_command(f"cut -f1,2 {genome_file}.fai > {chrom_sizes_file}")
+    # print("3\n")
+    bigwig_file = output_prefix + ".bw" 
+    # print("4\n")
+    run_command(f"bedGraphToBigWig {bedgraph_file} {chrom_sizes_file} {bigwig_file}")
+    # print("5\n")
+    return bigwig_file
+
+def fastq_to_bigwig(fastq_file, genome_file, aligner_path):
+    """Pipeline to convert FASTQ to BigWig."""
+    output_prefix = get_base_name(fastq_file)
+    create_directory(output_prefix)
+    output_prefix = os.path.join(output_prefix, output_prefix)
+    sam_file = align_fastq(fastq_file, genome_file, aligner_path, output_prefix)
+    bam_file = sam_to_bam(sam_file, output_prefix)
+    bedgraph_file = generate_bedgraph(bam_file, genome_file, output_prefix)
+    bigwig_file = bedgraph_to_bigwig(bedgraph_file, genome_file, output_prefix)
+    print(f"Done! BigWig file generated at: {bigwig_file}")
+
+def main():
+    parser = argparse.ArgumentParser(description="Convert FASTQ to BigWig.")
+    parser.add_argument("fastq_file", help="Path to the FASTQ file.")
+    parser.add_argument("genome_file", help="Path to the genome file.")
+    parser.add_argument("aligner_path", help="Path to the aligner executable.")
+    args = parser.parse_args()
+    # Use "bowtie2" for alignment 
+    fastq_to_bigwig(args.fastq_file, args.genome_file, args.aligner_path)
+    # E.g. usage: 
+    # python fastq_to_bigwig.py SRR20082645.fastq.gz resources/mm39/mm39 bowtie2
+
+if __name__ == "__main__":
+    main()
